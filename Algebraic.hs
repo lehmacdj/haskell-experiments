ff088304fdb14bd95771fb01e2417b62afb656b4
Devin Lehmacher <lehmacdj@gmail.com>
2016-11-26 14:17:08 -0500

    Begin implementing algebraic numbers

--- Algebraic.hs
+++ Algebraic.hs
@@ -4,22 +4,46 @@ module Rational
 
 import Data.Ratio
 
-newtype AlgebraicNumber = ANum [Rational]
+-- An algebraic number is a list of the coefficients of
+-- 1 + x + x^2 + x^3 + x^4 + ...
+-- if a term is missing it is implicitly 0
+-- except for the term just after the last term which is implicitly 1
+newtype AlgebraicNumber = ANum { getANum :: [Rational] }
+    deriving (Show)
+
+-- make the length be as small as possible
+cannonicalize :: AlgebraicNumber -> AlgebraicNumber
+cannonicalize = ANum . reverse . dropWhile (==0) . reverse . getANum
+
+-- make and cannonicalize
+makeANum :: [Rational] -> AlgebraicNumber
+makeANum = cannonicalize . ANum
+
+listWithLen :: AlgebraicNumber -> Int -> [Rational]
+listWithLen (ANum list) len
+    | length list >= len  = list
+    | length list < len   = listWithLen (ANum list) len
 
 -- Details on possible implementation details here
 -- http://reduce-algebra.com/docs/arnum.pdf
 instance Num AlgebraicNumber where
-    -- Add all terms except for the first
-    x + y = error "unimplemented"
+    -- Add all terms with one another
+    x + y =
+        let x' = listWithLen x (length $ getANum y)
+            y' = listWithLen y (length x')
+        in  makeANum $ zipWith (+) x' y'
     -- Multiply terms and then reduce
-    x * y = error "unimplemented"
-    negate x = error "unimplemented"
-    abs x = error "unimplemented"
-    signum x = error "unimplemented"
-    fromInteger i = [Term 1 1, Term 0 i]
+    x * y =
+        let x' = listWithLen x (length $ getANum y)
+            y' = listWithLen y (length x')
+        in  makeANum $ zipWith (*) x' y'
+    negate (ANum x) = makeANum $ map negate x
+    abs x = undefined
+    signum x = undefined
+    fromInteger i = ANum [-i % 1]
 
 instance Eq AlgebraicNumber where
-    (==) = error "unimplemented"
+    (==) = undefined
 
 instance Ord AlgebraicNumber where
-    (>) = error "unimplemented"
+    compare = undefined
