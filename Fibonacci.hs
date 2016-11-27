ee0f2a7d5ca246a83be3b9baa20e853c73f8af9e
Devin Lehmacher <lehmacdj@gmail.com>
2016-11-27 11:54:51 -0500

    Add memoization version of fibs

--- Fibonacci.hs
+++ Fibonacci.hs
@@ -1,4 +1,12 @@
 module Fibonacci where
+import Data.Function
 
 fibs :: [Integer]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+-- https://wiki.haskell.org/Memoization
+fib f 0 = 0
+fib f 1 = 1
+fib f n = f (n - 1) + f (n - 2)
+memoize f = (map f [0..] !!)
+fibonacci = fix (memoize . fib)
