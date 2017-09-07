cc359e08061e4109366635eab663d10e2f68a364
Devin Lehmacher <lehmacdj@gmail.com>
2017-09-07 14:49:55 -0400

    add type signatures

--- Fibonacci.hs
+++ Fibonacci.hs
@@ -1,12 +1,18 @@
 module Fibonacci where
 import Data.Function
+import Data.List (genericIndex)
 
 fibs :: [Integer]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 -- https://wiki.haskell.org/Memoization
-fib f 0 = 0
-fib f 1 = 1
+fib :: (Num t, Num a, Eq a) => (a -> t) -> a -> t
+fib _ 0 = 0
+fib _ 1 = 1
 fib f n = f (n - 1) + f (n - 2)
-memoize f = (map f [0..] !!)
+
+memoize :: (Num a, Enum a) => (a -> b) -> Int -> b
+memoize f = (map f [0..] `genericIndex`)
+
+fibonacci :: Int -> Integer
 fibonacci = fix (memoize . fib)
