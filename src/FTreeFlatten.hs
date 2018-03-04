9db4ad37c01ca9fc1347cf304051a8dea0d894d1
Devin Lehmacher <lehmacdj@gmail.com>
2018-03-04 00:57:40 -0500

    implement IR -> lowered IR translation

---
+++ src/FTreeFlatten.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module FTreeFlatten where
+
+import Data.Functor.Foldable
+import Data.Functor.Product
+import Control.Monad.Free
+import Data.Functor.Const
+import Control.Lens
+import Control.Monad.State
+import Control.Arrow
+
+type Temp = Integer
+type Label = Integer
+
+-- | Operators for IR. These are the same as the operators used by the Java
+-- interpreter.
+data Operator
+  = ADD | SUB | MUL | HMUL | DIV | MOD
+  | AND | OR | XOR
+  | LSHIFT | RSHIFT | ARSHIFT
+  | EQ | NEQ | LT | GT | LEQ | GEQ
+
+-- | Core types of nodes for IR expressions.
+data E e
+  = CONST Int
+  | TEMP Temp
+  | NAME Label
+  | MEM e
+  | OP Operator e e
+  | CALL e [e]
+  deriving (Functor)
+
+-- | Impure IR expressions.
+data EEff
+  = ERec (E EEff)
+  | ESEQ STree EEff
+
+-- | Expressions that do not contain side effects.
+-- * As an additional invariant CALL may not occur in EPure except where allowed
+-- by the the definition of SFlat.
+type EPure = Fix E
+
+-- | Core types of nodes for IR statements.
+data S e
+  = MOVE e e
+  | EXP e
+  | JUMP e
+  | CJUMP e Label Label
+  | LABEL Label
+  | RETURN
+  deriving (Functor)
+
+-- | Unflattened IR statements.
+data STree
+  = STip (S EEff)
+  | SEQ [STree]
+
+-- | Flattened/Lowered IR statements. There are a couple of additional
+-- invariants.
+-- * CALL is allowed as the first argument of MOVE
+-- * The only argument of EXP that is allowed is CALL
+type SFlat = [S EPure]
+
+-- | Experimental approaches that should be isomorphic to the respective
+-- version without a prime (modulo "fast and loose reasoning")
+type EEff' = Free (Product (Const [STree]) []) (E EEff)
+type STree' = Free [] (S EEff')
+
+-- Return a temporary location to be used for TEMP.
+getTemp :: MonadState Temp m => m Temp
+getTemp = modify (+1) >> get
+
+-- | Takes an EPure creates a MOVE statement that stores it and the TEMP it is
+-- stored to.
+storingTemp :: MonadState Temp m => EPure -> m (S EPure, EPure)
+storingTemp e = (flip MOVE e &&& id) . Fix . TEMP <$> getTemp
+
+-- | Constructs a SFlat that computes the first expression store it in a new
+-- TEMP then computes the second expression. Returns that SFlat the TEMP used
+-- for storing the first value and the pure expression constructed from the
+-- second expression.
+interleaveTemp :: MonadState Temp m => EEff -> EEff -> m (SFlat, Temp, EPure)
+interleaveTemp e1 e2 = do
+  (s1, e1') <- flattenEEff e1
+  (s2, e2') <- flattenEEff e2
+  temp <- getTemp
+  pure (s1 ++ [MOVE (Fix (TEMP temp)) e1'] ++ s2, temp, e2')
+
+flattenEEff :: MonadState Temp m => EEff -> m (SFlat, EPure)
+
+flattenEEff (ERec (CONST i)) = pure ([], Fix (CONST i))
+flattenEEff (ERec (NAME l)) = pure ([], Fix (NAME l))
+flattenEEff (ERec (TEMP t)) = pure ([], Fix (TEMP t))
+flattenEEff (ERec (MEM m)) = over (mapped._2) (Fix . MEM) (flattenEEff m)
+
+flattenEEff (ESEQ s e) = do
+  s' <- flattenSTree s
+  (se, e') <- flattenEEff e
+  pure (s' ++ se, e')
+
+flattenEEff (ERec (CALL f as)) = do
+  (ss, es') <- mapAndUnzipM flattenEEff (f:as)
+  (ms, tf:tas) <- mapAndUnzipM storingTemp es'
+  let ss' = concat $ zipWith ((reverse .) . (:)) ms ss
+  temp <- Fix . TEMP <$> getTemp
+  pure (ss' ++ [MOVE temp (Fix (CALL tf tas))], temp)
+
+-- TODO: optimize for case when e1 and e2 commute
+flattenEEff (ERec (OP o e1 e2)) = do
+  (s, temp, e') <- e1 `interleaveTemp` e2
+  pure (s, Fix (OP o (Fix (TEMP temp)) e'))
+
+flattenSTree :: MonadState Temp m => STree -> m SFlat
+
+flattenSTree (STip (EXP e)) = fst <$> flattenEEff e
+flattenSTree (STip (LABEL l)) = pure [LABEL l]
+flattenSTree (STip RETURN) = pure [RETURN]
+flattenSTree (SEQ ss) = concat <$> traverse flattenSTree ss
+
+flattenSTree (STip (JUMP e)) =
+  (\(s, e') -> s ++ [JUMP e']) <$> flattenEEff e
+
+flattenSTree (STip (CJUMP e l1 l2)) =
+  (\(s, e') -> s ++ [CJUMP e' l1 l2]) <$> flattenEEff e
+
+-- TODO: optimize for case when e1 and e2 commute
+flattenSTree (STip (MOVE e1 e2)) = do
+  (s, temp, e') <- e1 `interleaveTemp` e2
+  pure (s ++ [MOVE (Fix (MEM (Fix (TEMP temp)))) e'])
