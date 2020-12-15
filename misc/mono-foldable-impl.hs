02f96edd36581c0e7019ccd9551f004e7b89627d
Devin Lehmacher <lehmacdj@gmail.com>
2020-12-14 21:01:56 -0800

    fix issue

--- misc/mono-foldable-impl.hs
+++ misc/mono-foldable-impl.hs
@@ -30,6 +30,8 @@ data Test = Test {_a :: Int, _b :: Int}
 
 type instance Element Test = Int
 
+-- the trick is that each default-ed method needs an implementation, otherwise
+-- this works relatively normally
 instance MonoFoldable Test where
   -- ofoldMap :: Monoid m => (Int -> m) -> Test -> m
   ofoldMap f (Test a b) = f a <> f b
@@ -40,21 +42,25 @@ instance MonoFoldable Test where
   -- ofoldl' :: (a -> Int -> a) -> a -> Test -> a
   ofoldl' f acc (Test a b) = f (f acc a) b
 
+  ofoldr1Ex f (Test a b) = f a b
+
+  ofoldl1Ex' f (Test a b) = f a b
+
 newtype Test2 = Test2 {unTest2 :: ByteString}
 
 type instance Element Test2 = Word8
 
-instance MonoFoldable Test2 where
-  ofoldMap f = ofoldr (mappend . f) mempty
-  ofoldr f acc = S.foldr f acc . unTest2
-  ofoldl' f acc = S.foldl' f acc . unTest2
-  otoList = S.unpack . unTest2
-  oall f = S.all f . unTest2
-  oany f = S.any f . unTest2
-  onull = S.null . unTest2
-  olength = S.length . unTest2
-  oelem e = S.elem e . unTest2
-  onotElem e = S.notElem e . unTest2
+-- instance MonoFoldable Test2 where
+--   ofoldMap f = ofoldr (mappend . f) mempty
+--   ofoldr f acc = S.foldr f acc . unTest2
+--   ofoldl' f acc = S.foldl' f acc . unTest2
+--   otoList = S.unpack . unTest2
+--   oall f = S.all f . unTest2
+--   oany f = S.any f . unTest2
+--   onull = S.null . unTest2
+--   olength = S.length . unTest2
+--   oelem e = S.elem e . unTest2
+--   onotElem e = S.notElem e . unTest2
 
 main :: IO ()
 main = error "only intended for use in interactive ghci session"
