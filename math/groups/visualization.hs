df6db7dedb7715f058da79d503c97ff938390daa
Devin Lehmacher <lehmacdj@gmail.com>
2021-08-23 13:22:31 -0700

    implement some generic coloring

--- math/groups/visualization.hs
+++ math/groups/visualization.hs
@@ -5,26 +5,30 @@
     --package JuicyPixels
     --package directory
     --package classy-prelude
+    --package lens
  -}
 
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import ClassyPrelude
 import Codec.Picture
+import Codec.Picture.Types (Pixel16 (..))
+import Control.Lens (itraverse)
 import Data.Group
 import Data.Monoid (Sum (..))
+import Data.Word (Word16)
 import GHC.Stack (HasCallStack)
 import System.Directory
-import Prelude (showString, showParen)
+import Prelude (showParen, showString)
 
 perm :: HasCallStack => [Int] -> Sn
 perm xs
@@ -46,14 +50,15 @@ data Sn = Sigma
 
 instance Show Sn where
   showsPrec p = \case
-    Sigma {n=Nothing, ..} -> showString "identity"
-    e@Sigma {n=Just n, ..}
+    Sigma {n = Nothing, ..} -> showString "identity"
+    e@Sigma {n = Just n, ..}
       | e == mempty -> showString "identity"
-      | otherwise -> showParen (p > 10) $
+      | otherwise ->
+        showParen (p > 10) $
           showString "perm " . showsPrec 11 (fmap sigma [0 .. (n - 1)])
 
 instance Semigroup Sn where
-  Sigma {..} <> Sigma {sigma=sigma', n=n'}
+  Sigma {..} <> Sigma {sigma = sigma', n = n'}
     | isJust n && isJust n' && n /= n' = error "can't multiply elements of uneven length"
     | otherwise = Sigma (sigma . sigma') (n <|> n')
 
@@ -70,14 +75,14 @@ instance Group Sn where
         $ [0 .. n - 1] `zip` fmap sigma [0 .. n - 1]
 
 instance Eq Sn where
-  Sigma {n = Just n, ..} == Sigma {n = Just n', sigma=sigma'}
+  Sigma {n = Just n, ..} == Sigma {n = Just n', sigma = sigma'}
     | n /= n' = error "can't compare elements of uneven length"
-    | otherwise = fmap sigma [0 .. n-1] == fmap sigma' [0 .. n-1]
+    | otherwise = fmap sigma [0 .. n -1] == fmap sigma' [0 .. n -1]
   Sigma {n = Nothing} == Sigma {n = Nothing} = True
-  Sigma {n = Nothing} == Sigma {n = Just n, sigma = sigma}
-    = fmap sigma [0 .. n-1] == [0 .. n-1]
-  Sigma {n = Just n, sigma = sigma} == Sigma {n = Nothing}
-    = fmap sigma [0 .. n-1] == [0 .. n-1]
+  Sigma {n = Nothing} == Sigma {n = Just n, sigma = sigma} =
+    fmap sigma [0 .. n -1] == [0 .. n -1]
+  Sigma {n = Just n, sigma = sigma} == Sigma {n = Nothing} =
+    fmap sigma [0 .. n -1] == [0 .. n -1]
 
 instance Ord Sn where
   compare = compare `on` show
@@ -117,7 +122,11 @@ ensureDirExists = createDirectoryIfMissing True "images"
 pathOfName :: String -> String -> FilePath
 pathOfName name extension = "images/" ++ name ++ "." ++ extension
 
-render :: (Show m, Monoid m, Ord m) => FilePath -> [(m, PixelRGBA16)] -> IO ()
+render ::
+  (Pixel px, PngSavable px, Show m, Monoid m, Ord m) =>
+  FilePath ->
+  [(m, px)] ->
+  IO ()
 render name es = do
   ensureDirExists
   let image = mkImage es
@@ -127,8 +136,24 @@ black, white :: PixelRGBA16
 black = PixelRGBA16 0 0 0 maxBound
 white = PixelRGBA16 maxBound maxBound maxBound maxBound
 
+greyscaled :: [a] -> [(a, Pixel16)]
+greyscaled xs =
+  runIdentity $
+    itraverse
+      ( \i x ->
+          Identity
+            ( x,
+              let frac = fromIntegral i / fromIntegral len
+                  full = fromIntegral (maxBound :: Word16) :: Double
+               in round (frac * full)
+            )
+      )
+      xs
+  where
+    len = length xs
+
 main :: IO ()
 main = do
-  render "Z2-Test" [(Zero, black), (One, white)]
-  -- render "S4-LexicographicOrder" s4Elems
+  render "Z2-Greyscaled" (greyscaled [Zero, One])
+  render "S4-LexicographicOrder" (greyscaled (sort s4Elems))
   pure ()
